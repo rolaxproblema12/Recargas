beaf6e494d4f4da1444852225145a62245e56011
CesarSM <csarsm1709@hotmail.com>
2023-04-01 01:13:23 -0600

    Agregue pay/formPaymen Formulario de pago (domicilio de tarjeta)

---
+++ pay/formPayment.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<?php
+    if( isset($_POST['monto']) ){
+        $monto = $_POST['monto'];
+    }
+    if( isset($_POST['requestid']) ){
+        $requestid = isset($_POST['requestid']);
+    }
+    if( isset($_POST['currency'])){
+        $currency = $_POST['currency'];
+    }
+    
+?>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+
+    </form>
+<form method="post" action="https://gw-test.cgate.tech/orion/hosted/Payment.aspx" id="form1">
+<input type="hidden" name="Signature" id="Signature" value=" 863a288c6a395b017862b1432154591fb0ee78c8"/>
+<table>
+<tr>
+<td>Merchant Name:</td>
+<td><input name="MerchantName" id="MerchantName" type="text" value="Dummy1" readonly/></td>
+</tr>
+<tr>
+<td>Password:</td>
+<td><input name="MerchantPassword" id="MerchantPassword" type="text" value="p@s5w0Rd123" readonly /></td>
+</tr>
+<tr>
+<td>Merchant Ref:</td>
+<td><input name="MerchantRef" id="MerchantRef" type="text" value="130502203354" readonly/></td>
+</tr>
+<tr>
+<td>Currency:</td>
+<td><input name="Currency" id="Currency" type="text" value="USD" readonly/></td>
+</tr>
+<tr>
+<td>Amount:</td>
+<td><input name="Amount" id="Amount" type="text" value="2099" readonly/></td>
+</tr>
+<tr>
+<td>Success URL:</td>
+<td><input name="SuccessURL" id="SuccessURL" type="text" value="https://gw-test.cgate.tech/orion/tester/TestReturn.aspx" readonly/></td>
+</tr>
+<tr>
+<td>Fail URL:</td>
+<td><input name="FailURL" id="FailURL" type="text" value="https://gw-test.cgate.tech/orion/tester/TestReturn.aspx" readonly/></td>
+</tr>
+<tr>
+<td>Callback URL:</td>
+<td><input name="CallbackURL" id="CallbackURL" type="text" value="https://gw-test.cgate.tech/orion/tester/TestCallback.ashx" readonly/></td>
+</tr>
+
+
+<tr>
+    <td colspan="2">
+        <h3>Direccion de la tarjeta</h3>
+    </td>
+</tr>
+
+<tr>
+<td>Firstname:</td>
+<td><input name="Firstname" id="Firstname" type="text" value="" /></td>
+</tr>
+<tr>
+<td>Surname:</td>
+<td><input name="Surname" id="Surname" type="text" value="" /></td>
+</tr>
+<tr>
+<td>Street Line 1:</td>
+<td><input name="StreetLine1" id="StreetLine1" type="text" value="" /></td>
+</tr>
+<tr>
+<td>City:</td>
+<td><input name="City" id="City" type="text" value="" /></td>
+</tr>
+<tr>
+<td>Email:</td>
+<td><input name="Email" id="Email" type="text" value="" /></td>
+</tr>
+<tr>
+<td>Postal Code:</td>
+<td><input name="PostalCode" id="PostalCode" type="text" value="" /></td>
+</tr>
+<tr>
+<td>Telephone:</td>
+<td><input name="Telephone" id="Telephone" Telephone="text" value="" /></td>
+</tr>
+<tr>
+<td>State/Province:</td>
+<td><input name="StateProvince" id="StateProvince" type="text" value="" /></td>
+</tr>
+<tr>
+<td>Country:</td>
+<td><input name="Country" id="Country" type="text" value="" /></td>
+</tr>
+<tr>
+<td>Date Of Birth: (AAAA-MM-DD)</td>
+<td><input name="DateOfBirth" id="DateOfBirth" type="text" value="" /></td>
+</tr>
+
+<tr>
+<td style="text-align: right;">
+<input id="Submit1" type="submit" value="Submit"/>
+</td>
+</tr>
+</table>
+</form>
+
+
+        <!-- https://gw-test.cgate.tech/orion/hosted/Payment.aspx -->
+        <!-- 
+        <form action="https://gw-test.cgate.tech/orion/hosted/Payment.aspx" method="post">
+            <input type="text" id="MerchantName" name="MerchantName" value="Dummy1">
+            <input type="password" id="MerchantPassword" name="MerchantPassword" value="p@s5w0Rd123">
+            <button type="submit">Enviar</button>
+</form>
+<form method="post" action="https://gw-test.cgate.tech/orion/hosted/Payment.aspx" id="form1">
+<input type="hidden" name="Signature" id="Signature" value=" 863a288c6a395b017862b1432154591fb0ee78c8"/>
+<table>
+<tr>
+<td>Merchant Name:</td>
+<td><input name="MerchantName" id="MerchantName" type="text" value="Dummy1" /></td>
+</tr>
+<tr>
+<td>Firstname:</td>
+<td><input name="Firstname" id="Firstname" type="text" value="John" /></td>
+</tr>
+<tr>
+<td>Password:</td>
+<td><input name="MerchantPassword" id="MerchantPassword" type="text" value="p@s5w0Rd123" /></td>
+</tr>
+<tr>
+<td>Surname:</td>
+<td><input name="Surname" id="Surname" type="text" value="Phillips" /></td>
+</tr>
+<tr>
+<td>Merchant Ref:</td>
+<td><input name="MerchantRef" id="MerchantRef" type="text" value="130502203354" /></td>
+</tr>
+<tr>
+<td>Street Line 1:</td>
+<td><input name="StreetLine1" id="StreetLine1" type="text" value="92 West Broadway" /></td>
+</tr>
+<tr>
+<td>Currency:</td>
+<td><input name="Currency" id="Currency" type="text" value="USD" /></td>
+</tr>
+<tr>
+<td>Amount:</td>
+<td><input name="Amount" id="Amount" type="text" value="2099" /></td>
+</tr>
+<tr>
+<td>City:</td>
+<td><input name="City" id="City" type="text" value="New York" /></td>
+</tr>
+<tr>
+<td>Email:</td>
+<td><input name="Email" id="Email" type="text" value="john.phillips.1972@gmail.com" /></td>
+</tr>
+<tr>
+<td>Postal Code:</td>
+<td><input name="PostalCode" id="PostalCode" type="text" value="10007" /></td>
+</tr>
+<tr>
+<td>Telephone:</td>
+<td><input name="Telephone" id="Telephone" Telephone="text" value="2125661901" /></td>
+</tr>
+<tr>
+<td>State/Province:</td>
+12
+<td><input name="StateProvince" id="StateProvince" type="text" value="NY" /></td>
+</tr>
+<tr>
+<td>Country:</td>
+<td><input name="Country" id="Country" type="text" value="US" /></td>
+</tr>
+<tr>
+<td>Date Of Birth:</td>
+<td><input name="DateOfBirth" id="DateOfBirth" type="text" value="1972-08-18" /></td>
+</tr>
+<tr>
+<td>Success URL:</td>
+<td><input name="SuccessURL" id="SuccessURL" type="text" value="https://gw-test.cgate.tech/orion/tester/TestReturn.aspx" /></td>
+</tr>
+<tr>
+<td>Fail URL:</td>
+<td><input name="FailURL" id="FailURL" type="text" value="https://gw-test.cgate.tech/orion/tester/TestReturn.aspx" /></td>
+</tr>
+<tr>
+<td>Callback URL:</td>
+<td><input name="CallbackURL" id="CallbackURL" type="text" value="https://gw-test.cgate.tech/orion/tester/TestCallback.ashx" /></td>
+</tr>
+<tr>
+<td style="text-align: right;">
+<input id="Submit1" type="submit" value="Submit"/>
+</td>
+</tr>
+</table>
+</form>
+    
+    
+    
+    -->
+    </body>
+</html>
