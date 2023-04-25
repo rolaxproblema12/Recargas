404ec7970728d933e6125334e37a55666247b69b
CesarSM <csarsm1709@hotmail.com>
2023-04-25 00:58:31 -0600

    Pay status

--- js/payStatus.js
+++ js/payStatus.js
@@ -9,15 +9,40 @@ function optionsGetPurchaseStatus () {
     const optionPurchaseStatus = {
         method: 'POST',
         mode: 'no-cors',
-        body: datos
+        body: "merchant_id=sandbox&password=RZIuVE3NySIP9fQ&trans_id=1681960472&int_trans_id=1681960472&website_id=sandbox",
+        Headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+
     }
     return optionPurchaseStatus;
 };
 
-async function obtenerRespuesta(){
+function obtenerRespuesta(){
+  fetch(url, optionsGetPurchaseStatus())
+  .then(response => response.text())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+  /*
     const response = await fetch(url ,optionsGetPurchaseStatus());
     console.log(response); 
-    const datos = await response.text();
+    const datos = response.text();
     console.log(datos); 
+    */
+}
 
+function getRespuestaaa(){
+    const data = "merchant_id=sandbox&password=RZIuVE3NySIP9fQ&trans_id=1681960472&int_trans_id=1681960472&website_id=sandbox";
+
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === this.DONE) {
+        console.log(this.responseText);
+      }
+    });
+    
+    xhr.open("POST", "https://api.peak-pay.com/get-purchase-status", true);
+    xhr.send(data);
 }
