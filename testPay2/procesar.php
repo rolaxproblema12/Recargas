2a665192ecdba87c227df98a1d76e77b3b66e924
CesarSM <csarsm1709@hotmail.com>
2023-04-27 12:47:09 -0600

    Get Status paymen

--- testPay2/procesar.php
+++ testPay2/procesar.php
@@ -1,10 +1,23 @@
 <?php
-    $data = array('merchant_id' => 'value1', 'password' => 'value2');
+    $data = array(  'merchant_id' => $_POST['merchant_id'], 
+                    'password' => $_POST['password'] , 
+                    'trans_id' => $_POST['trans_id'], 
+                    'int_trans_id' => $_POST['int_trans_id'],
+                    'website_id' => $_POST['website_id']
+                );
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://api.example.com');
+    curl_setopt($ch, CURLOPT_URL, 'https://api.peak-pay.com/get-purchase-status');
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $response = curl_exec($ch);
     curl_close($ch);
+    
+    if($response == false){
+        echo("Error :(");
+    }
+    else{
+        $data = utf8_decode($response);
+        echo($data);
+    }
 ?>
